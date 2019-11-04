d3b4f71e29e7ea0299c37ee98daf91df4d3db6d8
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-11-03 20:26:07 -0700

    toggle abv function

--- src/app/main/customize-beer/customize-beer.component.ts
+++ src/app/main/customize-beer/customize-beer.component.ts
@@ -26,10 +26,19 @@ export class CustomizeBeerComponent implements OnInit {
     console.log('event: ', e)
     this.beersToDisplay = []
     for(var beer of this.allBeers) {
-      console.log(beer.abv)
-      if(beer.abv<(e.value+0.25) && beer.abv >(e.value-0.25)){
+      // console.log(beer.abv)
+      if(beer.abv<(e.value+0.5) && beer.abv >(e.value-0.5)){
         console.log('yes')
+        if(!this.beersToDisplay.includes(beer)) {
+          this.beersToDisplay.push(beer)
+        }
+        
+      } else {
+        if(this.beersToDisplay.includes(beer)) {
+          this.beersToDisplay.slice(this.beersToDisplay.indexOf(beer), 1)
+        }
       }
+      console.log(this.beersToDisplay)
     }
 
   }
